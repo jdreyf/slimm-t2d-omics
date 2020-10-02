62ae04478cd947b298c20ca05508107fa46e90f0
jonathan dreyfuss <jdreyf@gmail.com>
2020-10-02 13:30:19 -0400

    resubmit

--- R/val_mediate_script.R
+++ R/val_mediate_script.R
@@ -6,8 +6,10 @@ library(mediation)
 # Chunk 2
 nsim <- 100
 nsamp <- 50
+nsamp.small <- 15
 nperm <- 1000
 
-prop.sig.med <- Hitman:::sim_barfield(med.fcn = Hitman:::ezmediate, b1t2.v=c(0, 0.14, 0.39), nsim = nsim, 
-                                      nsamp = nsamp, sims=nperm)
-write.csv(prop.sig.med, paste0("./validation/sim_mediate_nsamp", nsamp, "_nsim", nsim, "_nperm", nperm, ".csv"), na="")
+ezmed <- Hitman:::ezmediate
+prop.sig.med <- Hitman:::sim_barfield(med.fnm = "ezmed", b1t2.v=c(0, 0.14, 0.39), nsim = nsim, 
+                                      nsamp = nsamp.small, sims=nperm)
+write.csv(prop.sig.med, paste0("./validation/sim_mediate_nsamp", nsamp.small, "_nsim", nsim, "_nperm", nperm, ".csv"), na="")
